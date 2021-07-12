ed38ed00d1fb62d69920a0a552fb9a5e585455c9
Zomer <zomerzxc.19@gmail.com>
2021-07-13 05:47:16 +0800

    Add ColorBox

---
+++ src/ColorBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ColorBox.css';
+
+class ColorBox extends Component {
+  render() {
+    return (
+      <div style={{ background: this.props.background }} className='ColorBox'>
+        <span>{this.props.name}</span>
+        <span>MORE</span>
+      </div>
+    );
+  }
+}
+
+export default ColorBox;
