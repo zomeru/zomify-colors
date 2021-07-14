8ce02cc1d97f864de46d2c7a22e60bb0717c5567
Zomer <zomerzxc.19@gmail.com>
2021-07-14 09:54:53 +0800

    Start styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -11,9 +11,7 @@ class PaletteList extends Component {
         <MiniPalette />
         <h1>Palette List</h1>
         {palettes.map(palette => (
-          <p>
-            <Link to={`palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
