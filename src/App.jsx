bba49e123dccfd93ca679e326795b8004aa1026b
Zomer <zomerzxc.19@gmail.com>
2021-07-17 15:09:53 +0800

    Save palettes to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,28 @@ import { generatePalette } from './helpers/colorHelpers';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.syncLocalStorage = this.syncLocalStorage.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(palette => palette.id === id);
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      'palettes',
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
